448c0b4aeeccbc58a027f85cc35dc5e748e19df0
git-rp <patilrahul000@gmail.com>
2022-12-20 19:16:18 +0530

    search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -5,12 +5,20 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
   });
 });
 
-fetch('http://localhost:3000/weather?address=pune').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data);
-    }
+const weatherform = document.querySelector('form');
+const searchAddress = document.querySelector('input');
+weatherform.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const location = searchAddress.value;
+  console.log('submit', location);
+  const url = 'http://localhost:3000/weather?address=' + location;
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data);
+      }
+    });
   });
 });
