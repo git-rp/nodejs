c9c3480557c1fa75c7cf6d7b74a25d3e8b212acb
git-rp <patilrahul000@gmail.com>
2022-12-20 19:26:38 +0530

    updating user interface to load data from backend

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,6 +7,9 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 
 const weatherform = document.querySelector('form');
 const searchAddress = document.querySelector('input');
+const msg1 = document.querySelector('#message-1');
+const msg2 = document.querySelector('#message-2');
+//msg1.textContent = 'Js msg';
 weatherform.addEventListener('submit', (event) => {
   event.preventDefault();
   const location = searchAddress.value;
@@ -16,8 +19,12 @@ weatherform.addEventListener('submit', (event) => {
     response.json().then((data) => {
       if (data.error) {
         console.log(data.error);
+        msg2.textContent = data.error;
       } else {
         console.log(data);
+        const { address, forcast } = data;
+        msg1.textContent =
+          'For location ' + address + ' The forcast for tommorow is ' + forcast;
       }
     });
   });
