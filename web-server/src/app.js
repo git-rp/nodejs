134b9cfb29b4ce34bfc2941ce332d056ce5d60eb
git-rp <patilrahul000@gmail.com>
2022-12-20 09:39:06 +0530

    html and json response

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,19 +2,30 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-  res.send('Hello Express...');
+  res.send('<h1>Hello Express...</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('help page');
+  res.send([
+    {
+      name: 'bhuvi',
+      age: 13,
+    },
+    {
+      name: 'Ten',
+      age: 14,
+    },
+  ]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('about page');
+  res.send('<h1>about page</h1>');
 });
 app.get('/weather', (req, res) => {
-  res.send('weather page');
+  res.send({ forcase: 'cloudy', location: 'pune' });
 });
+
+//Start server on port 3000 for listing request
 app.listen(3000, () => {
   console.log('server is up on port 3000');
 });
