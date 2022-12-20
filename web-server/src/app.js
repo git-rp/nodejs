e0b31456bb6179ac2b45f80727b9b1c3f3e145f9
git-rp <patilrahul000@gmail.com>
2022-12-20 11:56:15 +0530

    serving static assest ,html pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,31 @@
+const path = require('path');
 const express = require('express');
+console.log(__dirname);
+console.log();
 const app = express();
+const publicDir = path.join(__dirname, '../public');
+app.use(express.static(publicDir));
 
-app.get('', (req, res) => {
-  res.send('<h1>Hello Express...</h1>');
-});
+// app.get('', (req, res) => {
+//   res.send('<h1>Hello Express...</h1>');
+// });
 
-app.get('/help', (req, res) => {
-  res.send([
-    {
-      name: 'bhuvi',
-      age: 13,
-    },
-    {
-      name: 'Ten',
-      age: 14,
-    },
-  ]);
-});
+// app.get('/help', (req, res) => {
+//   res.send([
+//     {
+//       name: 'bhuvi',
+//       age: 13,
+//     },
+//     {
+//       name: 'Ten',
+//       age: 14,
+//     },
+//   ]);
+// });
 
-app.get('/about', (req, res) => {
-  res.send('<h1>about page</h1>');
-});
+// app.get('/about', (req, res) => {
+//   res.send('<h1>about page</h1>');
+// });
 app.get('/weather', (req, res) => {
   res.send({ forcase: 'cloudy', location: 'pune' });
 });
