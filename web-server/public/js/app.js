13cb944a930635a9a9f828d32262ac5af7355d17
git-rp <patilrahul000@gmail.com>
2022-12-20 19:04:22 +0530

    http request with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,16 @@
 console.log('client side js');
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
+
+fetch('http://localhost:3000/weather?address=pune').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data);
+    }
+  });
+});
