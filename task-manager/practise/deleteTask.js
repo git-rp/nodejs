2c9fa3bca22aaab783ed7104be12aee2d8d14b59
git-rp <patilrahul000@gmail.com>
2022-12-23 11:51:30 +0530

    deletetask

---
+++ task-manager/practise/deleteTask.js
@@ -0,0 +1,16 @@
+require('../src/db/mongoose');
+const Task = require('../src/model/task');
+
+deleteTaskAndCount = async (id, description) => {
+  const task = await Task.deleteOne({ _id: id });
+  const count = await Task.count({ description });
+  return count;
+};
+
+deleteTaskAndCount('63a548366b2486ef6a2a597b', 'Book complete')
+  .then((result) => {
+    console.log('Result ', result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
